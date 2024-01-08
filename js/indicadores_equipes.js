41fb512683ef1fdff28ca641a4e046c08f9f987a
APS Taiobeiras <apstaiobeiras@gmail.com>
2024-01-08 08:15:09 -0300

    Atualização

---
+++ js/indicadores_equipes.js
@@ -0,0 +1,273 @@
+const indicadores_equipes = `<tr>
+  <td><b>ACOLHER</b></td>
+  <td class="proximo right">55%</td>
+  <td class="right">2</td>
+  <td class="proximo right">88%</td>
+  <td class="right">1</td>
+  <td class="proximo right">88%</td>
+  <td class="right">1</td>
+  <td class="atingiu right">53%</td>
+  <td class="right"></td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">59%</td>
+  <td class="right"></td>
+  <td class="atingiu right">73%</td>
+  <td class="right"></td>
+</tr>
+<tr>
+  <td><b>CAMINHO</b></td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">57%</td>
+  <td class="right"></td>
+  <td class="proximo right">81%</td>
+  <td class="right">2</td>
+  <td class="atingiu right">67%</td>
+  <td class="right"></td>
+  <td class="atingiu right">70%</td>
+  <td class="right"></td>
+</tr>
+<tr>
+  <td><b>COMUN. RURAIS</b></td>
+  <td class="neutro right">0%</td>
+  <td class="right"></td>
+  <td class="neutro right">0%</td>
+  <td class="right"></td>
+  <td class="neutro right">0%</td>
+  <td class="right"></td>
+  <td class="atingiu right">53%</td>
+  <td class="right"></td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">59%</td>
+  <td class="right"></td>
+  <td class="proximo right">45%</td>
+  <td class="right">15</td>
+</tr>
+<tr>
+  <td><b>ESPERANÇA</b></td>
+  <td class="proximo right">66%</td>
+  <td class="right">1</td>
+  <td class="razoavel right">66%</td>
+  <td class="right">1</td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="proximo right">47%</td>
+  <td class="right">18</td>
+  <td class="proximo right">80%</td>
+  <td class="right">1</td>
+  <td class="atingiu right">57%</td>
+  <td class="right"></td>
+  <td class="atingiu right">61%</td>
+  <td class="right"></td>
+</tr>
+<tr>
+  <td><b>ESPETACULAR</b></td>
+  <td class="proximo right">71%</td>
+  <td class="right">1</td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">60%</td>
+  <td class="right"></td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">70%</td>
+  <td class="right"></td>
+  <td class="atingiu right">75%</td>
+  <td class="right"></td>
+</tr>
+<tr>
+  <td><b>HARMONIA</b></td>
+  <td class="razoavel right">42%</td>
+  <td class="right">3</td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">52%</td>
+  <td class="right"></td>
+  <td class="proximo right">88%</td>
+  <td class="right">1</td>
+  <td class="atingiu right">62%</td>
+  <td class="right"></td>
+  <td class="atingiu right">62%</td>
+  <td class="right"></td>
+</tr>
+<tr>
+  <td><b>HUMANIZAR</b></td>
+  <td class="atingiu right">81%</td>
+  <td class="right"></td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">56%</td>
+  <td class="right"></td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">62%</td>
+  <td class="right"></td>
+  <td class="atingiu right">59%</td>
+  <td class="right"></td>
+</tr>
+<tr>
+  <td><b>LIBERDADE</b></td>
+  <td class="proximo right">66%</td>
+  <td class="right">1</td>
+  <td class="razoavel right">66%</td>
+  <td class="right">3</td>
+  <td class="proximo right">77%</td>
+  <td class="right">2</td>
+  <td class="atingiu right">53%</td>
+  <td class="right"></td>
+  <td class="proximo right">80%</td>
+  <td class="right">2</td>
+  <td class="atingiu right">60%</td>
+  <td class="right"></td>
+  <td class="proximo right">43%</td>
+  <td class="right">23</td>
+</tr>
+<tr>
+  <td><b>MAIS SAÚDE</b></td>
+  <td class="proximo right">66%</td>
+  <td class="right">1</td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">50%</td>
+  <td class="right"></td>
+  <td class="proximo right">87%</td>
+  <td class="right">1</td>
+  <td class="atingiu right">64%</td>
+  <td class="right"></td>
+  <td class="atingiu right">65%</td>
+  <td class="right"></td>
+</tr>
+<tr>
+  <td><b>PROGRESSO</b></td>
+  <td class="atingiu right">90%</td>
+  <td class="right"></td>
+  <td class="proximo right">90%</td>
+  <td class="right">1</td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">53%</td>
+  <td class="right"></td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">60%</td>
+  <td class="right"></td>
+  <td class="atingiu right">60%</td>
+  <td class="right"></td>
+</tr>
+<tr>
+  <td><b>RENASCER</b></td>
+  <td class="proximo right">57%</td>
+  <td class="right">2</td>
+  <td class="proximo right">85%</td>
+  <td class="right">1</td>
+  <td class="razoavel right">57%</td>
+  <td class="right">3</td>
+  <td class="razoavel right">35%</td>
+  <td class="right">143</td>
+  <td class="proximo right">85%</td>
+  <td class="right">1</td>
+  <td class="proximo right">44%</td>
+  <td class="right">111</td>
+  <td class="proximo right">47%</td>
+  <td class="right">28</td>
+</tr>
+<tr>
+  <td><b>UNIÃO</b></td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="neutro right">0%</td>
+  <td class="right"></td>
+  <td class="atingiu right">54%</td>
+  <td class="right"></td>
+  <td class="proximo right">81%</td>
+  <td class="right">2</td>
+  <td class="atingiu right">59%</td>
+  <td class="right"></td>
+  <td class="atingiu right">57%</td>
+  <td class="right"></td>
+</tr>
+<tr>
+  <td><b>VIDA</b></td>
+  <td class="razoavel right">50%</td>
+  <td class="right">1</td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">52%</td>
+  <td class="right"></td>
+  <td class="proximo right">77%</td>
+  <td class="right">2</td>
+  <td class="proximo right">56%</td>
+  <td class="right">4</td>
+  <td class="proximo right">50%</td>
+  <td class="right">14</td>
+</tr>
+<tr>
+  <td><b>VIDA E SAÚDE</b></td>
+  <td class="ruim right">0%</td>
+  <td class="right">1</td>
+  <td class="ruim right">0%</td>
+  <td class="right">1</td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">55%</td>
+  <td class="right"></td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="atingiu right">66%</td>
+  <td class="right"></td>
+  <td class="atingiu right">72%</td>
+  <td class="right"></td>
+</tr>
+<tr>
+  <td><b>VIVER</b></td>
+  <td class="proximo right">70%</td>
+  <td class="right">1</td>
+  <td class="proximo right">70%</td>
+  <td class="right">3</td>
+  <td class="atingiu right">100%</td>
+  <td class="right"></td>
+  <td class="proximo right">46%</td>
+  <td class="right">26</td>
+  <td class="proximo right">88%</td>
+  <td class="right">1</td>
+  <td class="proximo right">50%</td>
+  <td class="right">40</td>
+  <td class="proximo right">52%</td>
+  <td class="right">9</td>
+</tr>
+<tr>
+  <td><b>Município</b></td>
+  <td class="proximo right">62%</td>
+  <td class="right">17</td>
+  <td class="proximo right">83%</td>
+  <td class="right">15</td>
+  <td class="proximo right">87%</td>
+  <td class="right">10</td>
+  <td class="proximo right">48%</td>
+  <td class="right">213</td>
+  <td class="proximo right">89%</td>
+  <td class="right">7</td>
+  <td class="proximo right">55%</td>
+  <td class="right">161</td>
+  <td class="proximo right">54%</td>
+  <td class="right">86</td>
+</tr>
+`;
