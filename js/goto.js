cae7014339f218d791c4331a90aee6083c7f5b12
APS Taiobeiras <apstaiobeiras@gmail.com>
2024-01-09 15:13:30 -0300

    Atualização

--- js/goto.js
+++ js/goto.js
@@ -1,9 +1,45 @@
 function gotoPage(page) {
-    let cod      = (new URLSearchParams(window.location.search)).get('cod');
-    let token    = (new Date()).setSeconds((new Date()).getSeconds()+30).toString(36)
-    let mode     = (new URLSearchParams(window.location.search)).get('mode');
-    let unidade  = (new URLSearchParams(window.location.search)).get('unidade');
-    let backlink = window.location.href.replace(/^.*\//,"").replace(/\?.*/,"");
+    let cod       = (new URLSearchParams(window.location.search)).get('cod');
+    let token     = (new Date()).setSeconds((new Date()).getSeconds()+30).toString(36)
+    let mode      = (new URLSearchParams(window.location.search)).get('mode');
+    let unidade   = (new URLSearchParams(window.location.search)).get('unidade');
+    let home_link = (new URLSearchParams(window.location.search)).get('home_link');
+    let old_cod   = (new URLSearchParams(window.location.search)).get('old_cod');
+    let backlink  = window.location.href.replace(/^.*\//,"").replace(/\?.*/,"");
 
-    window.location.href = `${page}?cod=${cod}&token=${token}&mode=${mode}&unidade=${unidade}&backlink=${backlink}`;
+    window.location.href = `${page}?cod=${cod}&token=${token}&mode=${mode}&unidade=${unidade}&backlink=${backlink}&home_link=${home_link}&old_cod=${old_cod}`;
+}
+
+function backToHome(link) {
+    onclick = "window.location.href = 'acs.html?cod='+(new URLSearchParams(window.location.search)).get('cod')+'&token='+(new Date()).setSeconds((new Date()).getSeconds()+30).toString(36)";
+
+    let cod       = (new URLSearchParams(window.location.search)).get('cod');
+    let token     = (new Date()).setSeconds((new Date()).getSeconds()+30).toString(36)
+    let mode      = (new URLSearchParams(window.location.search)).get('mode');
+    let unidade   = (new URLSearchParams(window.location.search)).get('unidade');
+    let home_link = (new URLSearchParams(window.location.search)).get('home_link');
+    let old_cod   = (new URLSearchParams(window.location.search)).get('old_cod');
+    let backlink  = window.location.href.replace(/^.*\//,"").replace(/\?.*/,"");
+
+
+    if (link == "index.html" && (home_link != "null" || home_link != null)) {
+        home_link = home_link == "null"?"index.html":home_link;
+
+        window.location.href = `${home_link}?cod=${old_cod}&token=${token}&mode=${mode}`;
+        return;
+    }
+
+    window.location.href = `${link}?cod=${cod}&token=${token}&mode=${mode}&unidade=${unidade}&backlink=${backlink}&home_link=${home_link}&old_cod=${old_cod}`;
+}
+
+function bypassCPF(page,new_cod){
+    let cod       = (new URLSearchParams(window.location.search)).get('cod');
+    let token     = (new Date()).setSeconds((new Date()).getSeconds()+30).toString(36)
+    let mode      = (new URLSearchParams(window.location.search)).get('mode');
+
+    let backlink  = window.location.href.replace(/^.*\//,"").replace(/\?.*/,"");
+
+    let enc_cod = new_cod.toString(36);
+
+    window.location.href = `${page}?cod=${enc_cod}&token=${token}&mode=${mode}&backlink=${backlink}&home_link=${backlink}&old_cod=${cod}`;
 }
