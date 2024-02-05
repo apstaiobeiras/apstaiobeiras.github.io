fc7a1e4ed740086f69162733c1d54ba814c8b653
APS Taiobeiras <apstaiobeiras@gmail.com>
2024-02-05 16:52:02 -0300

    Atualização

---
+++ js/farmaceuticos_esf.js
@@ -0,0 +1,8 @@
+
+const farmaceuticos_esf = [];
+farmaceuticos_esf['1204'] = ['HARMONIA'];
+farmaceuticos_esf['1108'] = ['UNIÂO'];
+farmaceuticos_esf['1196'] = ['ACOLHER, MAIS SAÙDE'];
+farmaceuticos_esf['1213'] = ['HUMANIZAR”, “ESPETACULAR'];
+farmaceuticos_esf['1037'] = ['VIDA E SAÙDE'];
+farmaceuticos_esf['1177'] = ['ESPERANÇA, CAMINHO'];
