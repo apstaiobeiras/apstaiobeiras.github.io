43fd09e5037eb686ccd0cbe031c37bf42546e0c8
APS Taiobeiras <apstaiobeiras@gmail.com>
2024-02-15 07:01:03 -0300

    Atualização

---
+++ js/lista_ace.js
@@ -0,0 +1,96 @@
+  <table>
+    <tr>
+      <th></th><th>Nome ACE</th><th>Visitas</th><th>Imóveis</th><th>Percentual</th>
+    </tr>
+    <tr class="tr" name="CAMINHO">
+      <td><b>120</b></td>
+      <td>Adimar Bandeira </td>
+      <td>727</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr" name="MAIS SAÚDE">
+      <td><b>124</b></td>
+      <td>Alan Carlos Ferreira</td>
+      <td>1264</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr" name="HUMANIZAR">
+      <td><b>56</b></td>
+      <td>Caique Rodrigues</td>
+      <td>558</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr" name="ACOLHER">
+      <td><b>124</b></td>
+      <td>Daniel de Jesus</td>
+      <td>1383</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr" name="VIDA">
+      <td><b>123</b></td>
+      <td>Daniel Lima</td>
+      <td>1338</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr" name="PROGRESSO">
+      <td><b>116</b></td>
+      <td>Dione Ferraz</td>
+      <td>1268</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr" name="LIBERDADE">
+      <td><b>116</b></td>
+      <td>Divone Ferraz</td>
+      <td>1168</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr" name="CAMINHO">
+      <td><b>120</b></td>
+      <td>Evila Miriam Agostinho</td>
+      <td>1594</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr" name="VIDA E SAÚDE">
+      <td><b>121</b></td>
+      <td>Flavio Almeida</td>
+      <td>1621</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr" name="ESPERANÇA">
+      <td><b>120</b></td>
+      <td>Gabriel Dias</td>
+      <td>1307</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr" name="VIVER">
+      <td><b>122</b></td>
+      <td>Moises de Oliveira</td>
+      <td>1588</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr" name="UNIÃO">
+      <td><b>127</b></td>
+      <td>Selma Barbosa</td>
+      <td>89</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr" name="RENASCER">
+      <td><b>58</b></td>
+      <td>Vittor Gabriel Rodrigues</td>
+      <td>139</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+  </table>
---
+++ js/lista_ace_tabela.js
@@ -0,0 +1,98 @@
+let lista_dos_ace = `
+  <table>
+    <tr>
+      <th></th><th>Nome ACE</th><th>Visitas</th><th>Imóveis</th><th>Percentual</th>
+    </tr>
+    <tr class="tr ace" name="CAMINHO">
+      <td><b>120</b></td>
+      <td>Adimar Bandeira </td>
+      <td>727</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr ace" name="MAIS SAÚDE">
+      <td><b>124</b></td>
+      <td>Alan Carlos Ferreira</td>
+      <td>1264</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr ace" name="HUMANIZAR">
+      <td><b>56</b></td>
+      <td>Caique Rodrigues</td>
+      <td>558</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr ace" name="ACOLHER">
+      <td><b>124</b></td>
+      <td>Daniel de Jesus</td>
+      <td>1383</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr ace" name="VIDA">
+      <td><b>123</b></td>
+      <td>Daniel Lima</td>
+      <td>1338</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr ace" name="PROGRESSO">
+      <td><b>116</b></td>
+      <td>Dione Ferraz</td>
+      <td>1268</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr ace" name="LIBERDADE">
+      <td><b>116</b></td>
+      <td>Divone Ferraz</td>
+      <td>1168</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr ace" name="CAMINHO">
+      <td><b>120</b></td>
+      <td>Evila Miriam Agostinho</td>
+      <td>1594</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr ace" name="VIDA E SAÚDE">
+      <td><b>121</b></td>
+      <td>Flavio Almeida</td>
+      <td>1621</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr ace" name="ESPERANÇA">
+      <td><b>120</b></td>
+      <td>Gabriel Dias</td>
+      <td>1307</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr ace" name="VIVER">
+      <td><b>122</b></td>
+      <td>Moises de Oliveira</td>
+      <td>1588</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr ace" name="UNIÃO">
+      <td><b>127</b></td>
+      <td>Selma Barbosa</td>
+      <td>89</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+    <tr class="tr ace" name="RENASCER">
+      <td><b>58</b></td>
+      <td>Vittor Gabriel Rodrigues</td>
+      <td>139</td>
+      <td>0</td>
+      <td>inf%</td>
+    </tr>
+  </table>
+`; 
