d2660a476c0a783d88e96476c8f60d8544d928c0
APS Taiobeiras <apstaiobeiras@gmail.com>
2024-02-20 16:16:09 -0300

    Atualização

--- js/lista_ace_tabela.js
+++ js/lista_ace_tabela.js
@@ -1,7 +1,71 @@
 const lista_dos_ace1 = `
   <table>
     <tr>
-      <th></th><th>Nome ACE</th><th>Visitas</th><th>Imóveis</th><th>Percentual</th>
+      <th></th><th>Nome ACE</th>
     </tr>
-
+    <tr class="tr ace" name="ACOLHER" onclick="bypassCPF('relatorio-visitas-por-dia-por-imovel.html','w1')">
+      <td><b>---</b></td>
+      <td>Daniel de Jesus</td>
+    </tr>
+      <tr class="tr ace" name="UNIÃO" onclick="bypassCPF('relatorio-visitas-por-dia-por-imovel.html','gc')">
+      <td><b>---</b></td>
+      <td>Selma Barbosa</td>
+    </tr>
+      <tr class="tr ace" name="CAMINHO" onclick="bypassCPF('relatorio-visitas-por-dia-por-imovel.html','2bc')">
+      <td><b>---</b></td>
+      <td>Evila Miriam Agostinho</td>
+    </tr>
+      <tr class="tr ace" name="VIVER" onclick="bypassCPF('relatorio-visitas-por-dia-por-imovel.html','2be')">
+      <td><b>---</b></td>
+      <td>Moises de Oliveira</td>
+    </tr>
+      <tr class="tr ace" name="ESPERANÇA" onclick="bypassCPF('relatorio-visitas-por-dia-por-imovel.html','w7')">
+      <td><b>---</b></td>
+      <td>Gabriel Dias</td>
+    </tr>
+      <tr class="tr ace" name="equipe" onclick="bypassCPF('relatorio-visitas-por-dia-por-imovel.html','cod_profissinal')">
+      <td><b>---</b></td>
+      <td>ace</td>
+    </tr>
+      <tr class="tr ace" name="VIDA E SAÚDE" onclick="bypassCPF('relatorio-visitas-por-dia-por-imovel.html','5f')">
+      <td><b>---</b></td>
+      <td>Flavio Almeida</td>
+    </tr>
+      <tr class="tr ace" name="LIBERDADE" onclick="bypassCPF('relatorio-visitas-por-dia-por-imovel.html','56')">
+      <td><b>---</b></td>
+      <td>Divone Ferraz</td>
+    </tr>
+      <tr class="tr ace" name="PROGRESSO" onclick="bypassCPF('relatorio-visitas-por-dia-por-imovel.html','57')">
+      <td><b>---</b></td>
+      <td>Dione Ferraz</td>
+    </tr>
+      <tr class="tr ace" name="VIDA" onclick="bypassCPF('relatorio-visitas-por-dia-por-imovel.html','wb')">
+      <td><b>---</b></td>
+      <td>Daniel Lima</td>
+    </tr>
+      <tr class="tr ace" name="RENASCER" onclick="bypassCPF('relatorio-visitas-por-dia-por-imovel.html','w2')">
+      <td><b>---</b></td>
+      <td>Vittor Gabriel Rodrigues</td>
+    </tr>
+      <tr class="tr ace" name="HUMANIZAR" onclick="bypassCPF('relatorio-visitas-por-dia-por-imovel.html','gu')">
+      <td><b>---</b></td>
+      <td>Caique Rodrigues</td>
+    </tr>
+      <tr class="tr ace" name="CAMINHO" onclick="bypassCPF('relatorio-visitas-por-dia-por-imovel.html','in')">
+      <td><b>---</b></td>
+      <td>Adimar Bandeira </td>
+    </tr>
+      <tr class="tr ace" name="VIDA E SAÚDE" onclick="bypassCPF('relatorio-visitas-por-dia-por-imovel.html','5f')">
+      <td><b>---</b></td>
+      <td>Flavio de Almeida</td>
+    </tr>
+      <tr class="tr ace" name="MAIS SAÚDE" onclick="bypassCPF('relatorio-visitas-por-dia-por-imovel.html','w8')">
+      <td><b>---</b></td>
+      <td>Alan Carlos Ferreira</td>
+    </tr>
+      <tr class="tr ace" name="ESPETACULAR" onclick="bypassCPF('relatorio-visitas-por-dia-por-imovel.html','2bd')">
+      <td><b>---</b></td>
+      <td>Marcio Neres Pereira</td>
+    </tr>
+  
 `; 
