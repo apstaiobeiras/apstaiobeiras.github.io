9290adde7ec8811c1b93ac10b6d1ec0c7144ba87
Meu Nome <sudo-give-me-coffee@users.noreply.github.com>
2024-05-16 16:17:24 -0300

    Atualização 2024/05/16

--- js/calendario-ace.js
+++ js/calendario-ace.js
@@ -6,55 +6,55 @@ visitas_a_imoveis_por_dia['671'][3] = ["","","","","","35","---","---","30","33"
 visitas_a_imoveis_por_dia['671'][4] = ["","35","33","34","31","36","---","---","29","38","34","39","36","---","---","30","31","32","30","31","---","---","33","32","29","RG","RG","---","---","---","---"];
 
 visitas_a_imoveis_por_dia['1160'] = [];
-visitas_a_imoveis_por_dia['1160'][1] = ["","FERIADO","23","29","27","32","---","---","31","30","27","31","32","---","---","27","25","30","29","38","---","---","29","FERIADO","FERIADO","FERIADO","FERIADO","---","---","25","31","25"];
+visitas_a_imoveis_por_dia['1160'][1] = ["","FERIADO","23","29","27","32","---","---","31","30","27","31","32","---","---","27","25","30","29","38","---","---","29","LIRAa","LIRAa","LIRAa","LIRAa","---","---","25","31","25"];
 visitas_a_imoveis_por_dia['1160'][2] = ["","","","","26","22","---","---","29","23","30","29","24","---","---","FERIADO","FERIADO","FERIADO","29","FOLGA","---","---","30","23","25","23","24","---","---","32","26","27","24"];
 visitas_a_imoveis_por_dia['1160'][3] = ["","","","","","22","---","---","29","25","24","28","27","---","---","33","39","37","35","31","---","---","42","36","37","37","38","---","---","17","29","37","35","33","---","---"];
 visitas_a_imoveis_por_dia['1160'][4] = ["","34","31","27","29","26","---","---","24","36","37","34","ATESTADO","---","---","ATESTADO","ATESTADO","ATESTADO","ATESTADO","ATESTADO","---","---","ATESTADO","ATESTADO","ATESTADO","ATESTADO","19","---","---","---","---"];
 
 visitas_a_imoveis_por_dia['606'] = [];
-visitas_a_imoveis_por_dia['606'][1] = ["","FERIADO","29","23","32","29","---","---","31","28","32","24","33","---","---","24","34","25","22","21","---","---","30","FERIADO","FERIADO","FERIADO","FERIADO","---","---","38","26","32"];
+visitas_a_imoveis_por_dia['606'][1] = ["","FERIADO","29","23","32","29","---","---","31","28","32","24","33","---","---","24","34","25","22","21","---","---","30","LIRAa","LIRAa","LIRAa","LIRAa","---","---","38","26","32"];
 visitas_a_imoveis_por_dia['606'][2] = ["","","","","30","21","---","---","23","31","18","36","23","---","---","FERIADO","FERIADO","FERIADO","25","26","---","---","34","30","33","23","33","---","---","FOLGA","18","ATESTADO","ATESTADO"];
 visitas_a_imoveis_por_dia['606'][3] = ["","","","","","ATESTADO","---","---","17","32","31","31","29","---","---","19","28","29","27","24","---","---","28","29","27","27","28","---","---","28","26","23","19","26","---","---"];
 visitas_a_imoveis_por_dia['606'][4] = ["","31","23","33","24","39","---","---","36","33","20","29","35","---","---","27","23","33","27","22","---","---","31","29","25","22","24","---","---","---","---"];
 
 visitas_a_imoveis_por_dia['1163'] = [];
-visitas_a_imoveis_por_dia['1163'][1] = ["","FERIADO","41","37","39","38","---","---","38","30","33","34","31","---","---","35","36","23","46","41","---","---","42","FERIADO","FERIADO","FERIADO","FERIADO","---","---","32","31","29"];
+visitas_a_imoveis_por_dia['1163'][1] = ["","FERIADO","41","37","39","38","---","---","38","30","33","34","31","---","---","35","36","23","46","41","---","---","42","LIRAa","LIRAa","LIRAa","LIRAa","---","---","32","31","29"];
 visitas_a_imoveis_por_dia['1163'][2] = ["","","","","30","31","---","---","FOLGA","40","40","36","39","---","---","FERIADO","FERIADO","FERIADO","30","33","---","---","39","37","35","31","38","---","---","40","36","39","39"];
 visitas_a_imoveis_por_dia['1163'][3] = ["","","","","","41","---","---","43","44","35","41","39","---","---","36","48","46","43","44","---","---","33","37","35","37","35","---","---","33","45","30","20","48","---","---"];
 visitas_a_imoveis_por_dia['1163'][4] = ["","42","ATESTADO","ATESTADO","ATESTADO","FOLGA","---","---","36","35","38","37","37","---","---","35","38","38","41","38","---","---","34","39","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","---","---"];
 
 visitas_a_imoveis_por_dia['1153'] = [];
-visitas_a_imoveis_por_dia['1153'][1] = ["","FERIADO","38","22","35","31","---","---","28","26","32","29","33","---","---","30","28","34","FOLGA","FOLGA","---","---","FOLGA","FOLGA","FOLGA","FOLGA","FOLGA","---","---","30","29","28"];
+visitas_a_imoveis_por_dia['1153'][1] = ["","FERIADO","38","22","35","31","---","---","28","26","32","29","33","---","---","30","28","34","FOLGA","FOLGA","---","---","FOLGA","LIRAa","LIRAa","LIRAa","LIRAa","---","---","30","29","28"];
 visitas_a_imoveis_por_dia['1153'][2] = ["","","","","30","27","---","---","36","30","28","32","31","---","---","FERIADO","FERIADO","FERIADO","35","30","---","---","30","30","29","26","30","---","---","24","30","32","31"];
 visitas_a_imoveis_por_dia['1153'][3] = ["","","","","","29","---","---","ATESTADO","ATESTADO","ATESTADO","ATESTADO","39","---","---","38","37","41","30","38","---","---","32","37","39","36","33","---","---","34","34","36","31","37","---","---"];
 visitas_a_imoveis_por_dia['1153'][4] = ["","30","29","38","36","32","---","---","34","35","34","28","29","---","---","ATESTADO","ATESTADO","ATESTADO","ATESTADO","ATESTADO","---","---","37","35","33","34","36","---","---","---","---"];
 
 visitas_a_imoveis_por_dia['187'] = [];
-visitas_a_imoveis_por_dia['187'][1] = ["","FERIAS","FERIAS","FERIAS","FERIAS","FERIAS","---","---","FERIAS","FERIAS","FERIAS","FERIAS","FERIAS","---","---","FERIAS","FERIAS","FERIAS","46","23","---","---","34","FERIAS","FERIAS","FERIAS","FERIAS","---","---","33","33","31"];
+visitas_a_imoveis_por_dia['187'][1] = ["","FERIAS","FERIAS","FERIAS","FERIAS","FERIAS","---","---","FERIAS","FERIAS","FERIAS","FERIAS","FERIAS","---","---","FERIAS","FERIAS","FERIAS","46","23","---","---","34","LIRAa","LIRAa","LIRAa","LIRAa","---","---","33","33","31"];
 visitas_a_imoveis_por_dia['187'][2] = ["","","","","25","32","---","---","36","42","41","41","34","---","---","FERIADO","FERIADO","FERIADO","33","34","---","---","47","32","31","36","29","---","---","34","33","33","27"];
 visitas_a_imoveis_por_dia['187'][3] = ["","","","","","24","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---"];
 visitas_a_imoveis_por_dia['187'][4] = ["","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","---","---"];
 
 visitas_a_imoveis_por_dia['186'] = [];
-visitas_a_imoveis_por_dia['186'][1] = ["","FERIAS","FERIAS","FERIAS","FERIAS","FERIAS","---","---","FERIAS","FERIAS","FERIAS","FERIAS","FERIAS","---","---","FERIAS","FERIAS","FERIAS","39","40","---","---","32","FERIAS","FERIAS","FERIAS","FERIAS","---","---","34","34","42"];
+visitas_a_imoveis_por_dia['186'][1] = ["","FERIAS","FERIAS","FERIAS","FERIAS","FERIAS","---","---","FERIAS","FERIAS","FERIAS","FERIAS","FERIAS","---","---","FERIAS","FERIAS","FERIAS","39","40","---","---","32","LIRAa","LIRAa","LIRAa","LIRAa","---","---","34","34","42"];
 visitas_a_imoveis_por_dia['186'][2] = ["","","","","23","33","---","---","30","38","35","27","41","---","---","FERIADO","FERIADO","FERIADO","38","46","---","---","39","37","35","31","42","---","---","36","37","37","38"];
 visitas_a_imoveis_por_dia['186'][3] = ["","","","","","17","---","---","41","38","34","39","41","---","---","39","37","36","39","42","---","---","38","38","46","39","37","---","---","35","31","42","36","42","---","---"];
 visitas_a_imoveis_por_dia['186'][4] = ["","41","42","41","33","32","---","---","35","34","31","29","31","---","---","37","35","33","36","38","---","---","38","37","42","41","33","---","---","---","---"];
 
 visitas_a_imoveis_por_dia['1269'] = [];
-visitas_a_imoveis_por_dia['1269'][1] = ["","FERIADO","FOLGA","37","41","42","---","---","42","45","38","30","33","---","---","18","31","30","31","32","---","---","31","FOLGA","FOLGA","FOLGA","FOLGA","---","---","33","36","38"];
+visitas_a_imoveis_por_dia['1269'][1] = ["","FERIADO","FOLGA","37","41","42","---","---","42","45","38","30","33","---","---","18","31","30","31","32","---","---","31","LIRAa","LIRAa","LIRAa","LIRAa","---","---","33","36","38"];
 visitas_a_imoveis_por_dia['1269'][2] = ["","","","","24","36","---","---","35","35","32","31","29","---","---","FERIADO","FERIADO","FERIADO","36","39","---","---","28","27","34","34","36","---","---","24","32","37","29"];
 visitas_a_imoveis_por_dia['1269'][3] = ["","","","","","38","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---"];
 visitas_a_imoveis_por_dia['1269'][4] = ["","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","---","---"];
 
 visitas_a_imoveis_por_dia['195'] = [];
-visitas_a_imoveis_por_dia['195'][1] = ["","FERIADO","32","40","34","32","---","---","32","29","32","35","34","---","---","31","32","35","34","32","---","---","23","FERIADO","FERIADO","FERIADO","FERIADO","---","---","40","34","39"];
+visitas_a_imoveis_por_dia['195'][1] = ["","FERIADO","32","40","34","32","---","---","32","29","32","35","34","---","---","31","32","35","34","32","---","---","23","LIRAa","LIRAa","LIRAa","LIRAa","---","---","40","34","39"];
 visitas_a_imoveis_por_dia['195'][2] = ["","","","","34","40","---","---","37","25","39","32","37","---","---","FERIADO","FERIADO","FERIADO","37","31","---","---","38","34","39","31","39","---","---","37","36","39","32"];
 visitas_a_imoveis_por_dia['195'][3] = ["","","","","","38","---","---","40","39","34","32","42","---","---","41","33","31","36","39","---","---","58","47","34","34","39","---","---","37","36","39","42","38","---","---"];
 visitas_a_imoveis_por_dia['195'][4] = ["","41","38","34","39","41","---","---","36","35","38","40","36","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","32","33","29","---","---","---","---"];
 
 visitas_a_imoveis_por_dia['1159'] = [];
-visitas_a_imoveis_por_dia['1159'][1] = ["","FERIADO","34","21","33","22","---","---","31","32","34","28","30","---","---","33","18","31","23","22","---","---","29","FERIADO","FERIADO","FERIADO","FERIADO","---","---","34","21","25"];
+visitas_a_imoveis_por_dia['1159'][1] = ["","FERIADO","34","21","33","22","---","---","31","32","34","28","30","---","---","33","18","31","23","22","---","---","29","LIRAa","LIRAa","LIRAa","LIRAa","---","---","34","21","25"];
 visitas_a_imoveis_por_dia['1159'][2] = ["","","","","29","28","---","---","37","38","36","34","42","---","---","FERIADO","FERIADO","FERIADO","34","22","---","---","32","27","34","30","28","---","---","30","36","31","29"];
 visitas_a_imoveis_por_dia['1159'][3] = ["","","","","","34","---","---","34","42","42","37","39","---","---","37","35","31","42","36","---","---","37","37","38","36","33","---","---","FOLGA","FOLGA","FOLGA","FOLGA","FOLGA","---","---"];
 visitas_a_imoveis_por_dia['1159'][4] = ["","FOLGA","FOLGA","FOLGA","FOLGA","FOLGA","---","---","VIGILANCIA SANITÁRIA","VIGILANCIASANITÁRIA","VIGILANCIASANITÁRIA","VIGILANCIASANITÁRIA","VIGILANCIASANITÁRIA","---","---","VIGILANCIASANITÁRIA","VIGILANCIASANITÁRIA","VIGILANCIASANITÁRIA","VIGILANCIASANITÁRIA","VIGILANCIASANITÁRIA","---","---","VIGILANCIASANITÁRIA","VIGILANCIASANITÁRIA","VIGILANCIASANITÁRIA","VIGILANCIASANITÁRIA","VIGILANCIASANITÁRIA","---","---","---","---"];
@@ -66,7 +66,7 @@ visitas_a_imoveis_por_dia['468'][3] = ["","","","","","30","---","---","09","27"
 visitas_a_imoveis_por_dia['468'][4] = ["","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","---","---"];
 
 visitas_a_imoveis_por_dia['1270'] = [];
-visitas_a_imoveis_por_dia['1270'][1] = ["","FERIADO","21","14","33","21","---","---","23","27","14","12","21","---","---","18","14","16","28","16","---","---","34","FERIADO","FERIADO","FERIADO","FERIADO","---","---","16","19","8"];
+visitas_a_imoveis_por_dia['1270'][1] = ["","FERIADO","21","14","33","21","---","---","23","27","14","12","21","---","---","18","14","16","28","16","---","---","34","LIRAa","LIRAa","LIRAa","LIRAa","---","---","16","19","8"];
 visitas_a_imoveis_por_dia['1270'][2] = ["","","","","25","13","---","---","17","19","14","16","29","---","---","FERIADO","FERIADO","FERIADO","16","23","---","---","18","21","24","14","16","---","---","19","15","16","12"];
 visitas_a_imoveis_por_dia['1270'][3] = ["","","","","","24","---","---","24","28","29","ATESTADO","ATESTADO","---","---","21","16","14","33","23","---","---","18","21","24","23","16","---","---","23","18","21","24","14","---","---"];
 visitas_a_imoveis_por_dia['1270'][4] = ["","16","19","15","16","12","---","---","19","21","24","27","29","---","---","26","24","22","ATESTADO","ATESTADO","---","---","24","25","22","24","19","---","---","---","---"];
@@ -78,7 +78,7 @@ visitas_a_imoveis_por_dia['99991'][3] = ["","","","","","0","---","---","37","35
 visitas_a_imoveis_por_dia['99991'][4] = ["","33","36","35","34","37","---","---","31","38","37","42","41","---","---","33","31","25","32","33","---","---","31","RG","37","31","38","---","---","---","---"];
 
 visitas_a_imoveis_por_dia['1271'] = [];
-visitas_a_imoveis_por_dia['1271'][1] = ["","FERIADO","31","26","29","33","---","---","27","37","29","31","29","---","---","21","30","27","35","34","---","---","28","FERIADO","FERIADO","FERIADO","FERIADO","---","---","31","39","32"];
+visitas_a_imoveis_por_dia['1271'][1] = ["","FERIADO","31","26","29","33","---","---","27","37","29","31","29","---","---","21","30","27","35","34","---","---","28","LIRAa","LIRAa","LIRAa","LIRAa","---","---","31","39","32"];
 visitas_a_imoveis_por_dia['1271'][2] = ["","","","","29","33","---","---","37","36","31","32","30","---","---","FERIADO","FERIADO","FERIADO","30","29","---","---","34","32","32","31","33","---","---","24","35","31","29"];
 visitas_a_imoveis_por_dia['1271'][3] = ["","","","","","36","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---"];
 visitas_a_imoveis_por_dia['1271'][4] = ["","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","BLOQUEIO","---","---","---","---"];
